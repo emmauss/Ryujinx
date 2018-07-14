1810b17d7bd52873b0c7b4b06492cce699491cf9
emmaus <emmausssss@gmail.com>
2018-07-14 08:31:06 +0000

    implement IProfile Get(Base)

--- Ryujinx.HLE/Settings/SystemSettings.cs
+++ Ryujinx.HLE/Settings/SystemSettings.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using Ryujinx.HLE.OsHle;
+
 namespace Ryujinx.HLE.Settings
 {
     public class SystemSettings
     {
-        public ColorSet ThemeColor;
+        public Profile       ActiveUser { get; set; }
+        public ColorSet      ThemeColor { get; set; }
+        public List<Profile> UserProfiles { get; set; }
+        public int           DefaultUserIndex { get; set; }
     }
 }
