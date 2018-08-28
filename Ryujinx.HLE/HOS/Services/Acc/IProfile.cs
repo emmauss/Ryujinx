decb4c3ac3de41a8df11f66512f9078160caa520
emmaus <emmausssss@gmail.com>
2018-08-28 21:48:50 +0000

    implement loading of profile image

--- Ryujinx.HLE/HOS/Services/Acc/IProfile.cs
+++ Ryujinx.HLE/HOS/Services/Acc/IProfile.cs
@@ -3,7 +3,10 @@ using Ryujinx.HLE.HOS.Ipc;
 using Ryujinx.HLE.HOS.SystemState;
 using Ryujinx.HLE.Logging;
 using Ryujinx.HLE.Utilities;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace Ryujinx.HLE.HOS.Services.Acc
@@ -16,15 +19,21 @@ namespace Ryujinx.HLE.HOS.Services.Acc
 
         private UserProfile Profile;
 
+        private Stream IconStream;
+
         public IProfile(UserProfile Profile)
         {
             m_Commands = new Dictionary<int, ServiceProcessRequest>()
             {
-                { 0, Get     },
-                { 1, GetBase }
+                { 0,  Get          },
+                { 1,  GetBase      },
+                { 10, GetImageSize },
+                { 11, LoadImage    },
             };
 
             this.Profile = Profile;
+
+            IconStream = Assembly.GetCallingAssembly().GetManifestResourceStream("Ryujinx.HLE.Ryujinx_icon.jpg");
         }
 
         public long Get(ServiceCtx Context)
@@ -51,6 +60,29 @@ namespace Ryujinx.HLE.HOS.Services.Acc
             byte[] Username = StringUtils.GetFixedLengthBytes(Profile.Name, 0x20, Encoding.UTF8);
 
             Context.ResponseData.Write(Username);
+
+            return 0;
+        }
+
+        private long LoadImage(ServiceCtx Context)
+        {
+            long BufferPosition = Context.Request.ReceiveBuff[0].Position;
+            long BufferLen      = Context.Request.ReceiveBuff[0].Size;
+
+            byte[] IconData = new byte[BufferLen];
+
+            IconStream.Read(IconData, 0, IconData.Length);
+
+            Context.Memory.WriteBytes(BufferPosition, IconData);
+
+            Context.ResponseData.Write(IconStream.Length);
+
+            return 0;
+        }
+
+        private long GetImageSize(ServiceCtx Context)
+        {
+            Context.ResponseData.Write(IconStream.Length);
 
             return 0;
         }
