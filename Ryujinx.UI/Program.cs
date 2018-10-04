70553885f8370d30668649666814b4ef6f6cc0b1
emmaus <emmausssss@gmail.com>
2018-10-04 11:33:46 +0000

    base project

---
+++ Ryujinx.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ryujinx.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
