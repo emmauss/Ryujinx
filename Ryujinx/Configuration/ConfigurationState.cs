387a464a02c024b5a34cd98ee959511b1ae60514
Emmanuel Hansen <emmausssss@gmail.com>
2021-06-19 11:21:01 +0000

    add config

--- Ryujinx/Configuration/ConfigurationState.cs
+++ Ryujinx/Configuration/ConfigurationState.cs
@@ -836,6 +836,15 @@ namespace Ryujinx.Configuration
                 configurationFileUpdated = true;
             }
 
+            if (configurationFileFormat.Version < 27)
+            {
+                Common.Logging.Logger.Warning?.Print(LogClass.Application, $"Outdated configuration version {configurationFileFormat.Version}, migrating to version 27.");
+
+                configurationFileFormat.EnableMouse = false;
+
+                configurationFileUpdated = true;
+            }
+
             Logger.EnableFileLog.Value             = configurationFileFormat.EnableFileLog;
             Graphics.ResScale.Value                = configurationFileFormat.ResScale;
             Graphics.ResScaleCustom.Value          = configurationFileFormat.ResScaleCustom;
